61037fc9ed5a068f4d1d05819c713c205ed58e83
dokugo90 <dokugo90@gmail.com>
2022-12-19 22:50:22 +0000

    Fixed routing

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ import { HashRouter, BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>
+  </HashRouter>
 );
 
 
